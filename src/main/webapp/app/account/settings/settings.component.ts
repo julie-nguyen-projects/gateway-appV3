04cf0de1d6649375f9a6ffbb41cca12264e64e5b
Jerome Dobbels <jerome.dobbels@epitech.eu>
2019-05-27 12:05:54 +0200

    feat(user/userExtra): Error while loading userExtra parameters

--- src/main/webapp/app/account/settings/settings.component.ts
+++ src/main/webapp/app/account/settings/settings.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
-import { AccountService } from 'app/core';
+import { AccountService, IUser } from 'app/core';
+import { IUserExtra, UserExtra } from 'app/shared/model/user-extra.model';
+import { UserExtraService } from 'app/entities/user-extra';
+import { JhiAlertService, JhiDataUtils } from 'ng-jhipster';
+import { CityService } from 'app/entities/city';
+import { ICity } from 'app/shared/model/city.model';
+import { filter, map } from 'rxjs/operators';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-settings',
@@ -9,15 +16,33 @@ import { AccountService } from 'app/core';
 export class SettingsComponent implements OnInit {
     error: string;
     success: string;
-    settingsAccount: any;
+    settingsAccount: IUser;
+    userExtra: IUserExtra;
+    cities: ICity[];
     languages: any[];
 
-    constructor(private accountService: AccountService) {}
+    constructor(
+        protected dataUtils: JhiDataUtils,
+        protected jhiAlertService: JhiAlertService,
+        protected elementRef: ElementRef,
+        private accountService: AccountService,
+        protected cityService: CityService,
+        private userExtraService: UserExtraService
+    ) {}
 
     ngOnInit() {
         this.accountService.identity().then(account => {
             this.settingsAccount = this.copyAccount(account);
         });
+        this.userExtraService.find(this.settingsAccount.id).subscribe(
+            res => {
+                console.log(res);
+                this.userExtra = res.body;
+            },
+            err => {
+                this.onError(err.message);
+            }
+        );
     }
 
     save() {
@@ -28,6 +53,20 @@ export class SettingsComponent implements OnInit {
                 this.accountService.identity(true).then(account => {
                     this.settingsAccount = this.copyAccount(account);
                 });
+
+                this.userExtra.firstName = this.settingsAccount.firstName;
+                this.userExtra.lastName = this.settingsAccount.lastName;
+                this.userExtra.email = this.settingsAccount.email;
+                this.userExtraService.update(this.userExtra).subscribe(
+                    () => {
+                        this.error = null;
+                        this.success = 'OK';
+                    },
+                    () => {
+                        this.success = null;
+                        this.error = 'ERROR';
+                    }
+                );
             },
             () => {
                 this.success = null;
@@ -46,5 +85,39 @@ export class SettingsComponent implements OnInit {
             login: account.login,
             imageUrl: account.imageUrl
         };
+    }
+
+    byteSize(field) {
+        return this.dataUtils.byteSize(field);
+    }
+
+    openFile(contentType, field) {
+        return this.dataUtils.openFile(contentType, field);
+    }
+
+    setFileData(event, entity, field, isImage) {
+        this.dataUtils.setFileData(event, entity, field, isImage);
+    }
+
+    clearInputImage(field: string, fieldContentType: string, idInput: string) {
+        this.dataUtils.clearInputImage(this.userExtra, this.elementRef, field, fieldContentType, idInput);
+    }
+
+    trackCityById(index: number, item: ICity) {
+        return item.id;
+    }
+
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
+
+    search(event: any) {
+        this.cityService
+            .getCitiesByNameStartsWith(event.query)
+            .pipe(
+                filter((mayBeOk: HttpResponse<ICity[]>) => mayBeOk.ok),
+                map((response: HttpResponse<ICity[]>) => response.body)
+            )
+            .subscribe((res: ICity[]) => (this.cities = res), (res: HttpErrorResponse) => this.onError(res.message));
     }
 }
