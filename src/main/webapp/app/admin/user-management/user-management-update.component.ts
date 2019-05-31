feee2512a0637f16757d0636fd1fb9c72bdfd468
Jerome Dobbels <jerome.dobbels@epitech.eu>
2019-05-31 20:40:27 +0200

    feat(user/userExtra): Modification d'user par un administrateur + suppression du bouton edit de la liste userExtra

--- src/main/webapp/app/admin/user-management/user-management-update.component.ts
+++ src/main/webapp/app/admin/user-management/user-management-update.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { User, UserService } from 'app/core';
+import { UserExtraService } from 'app/entities/user-extra';
+import { UserExtra } from 'app/shared/model/user-extra.model';
+import { JhiAlertService, JhiDataUtils } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-user-mgmt-update',
@@ -9,16 +12,28 @@ import { User, UserService } from 'app/core';
 })
 export class UserMgmtUpdateComponent implements OnInit {
     user: User;
+    userExtra: UserExtra;
     languages: any[];
     authorities: any[];
     isSaving: boolean;
 
-    constructor(private userService: UserService, private route: ActivatedRoute, private router: Router) {}
+    constructor(
+        protected dataUtils: JhiDataUtils,
+        protected jhiAlertService: JhiAlertService,
+        protected elementRef: ElementRef,
+        private userService: UserService,
+        private userExtraService: UserExtraService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {}
 
     ngOnInit() {
         this.isSaving = false;
         this.route.data.subscribe(({ user }) => {
             this.user = user.body ? user.body : user;
+        });
+        this.userExtraService.find(this.user.id).subscribe(userExtra => {
+            this.userExtra = userExtra.body;
         });
         this.authorities = [];
         this.userService.authorities().subscribe(authorities => {
@@ -32,10 +47,16 @@ export class UserMgmtUpdateComponent implements OnInit {
 
     save() {
         this.isSaving = true;
+        this.userExtra.firstName = this.user.firstName;
+        this.userExtra.lastName = this.user.lastName;
+        this.userExtra.email = this.user.email;
+
         if (this.user.id !== null) {
+            this.userExtraService.update(this.userExtra).subscribe(response => this.onSaveSuccess(response), () => this.onSaveError());
             this.userService.update(this.user).subscribe(response => this.onSaveSuccess(response), () => this.onSaveError());
         } else {
             this.user.langKey = 'en';
+            this.userExtraService.create(this.userExtra).subscribe(response => this.onSaveSuccess(response), () => this.onSaveError());
             this.userService.create(this.user).subscribe(response => this.onSaveSuccess(response), () => this.onSaveError());
         }
     }
@@ -47,5 +68,21 @@ export class UserMgmtUpdateComponent implements OnInit {
 
     private onSaveError() {
         this.isSaving = false;
+    }
+
+    byteSize(field) {
+        return this.dataUtils.byteSize(field);
+    }
+
+    openFile(contentType, field) {
+        return this.dataUtils.openFile(contentType, field);
+    }
+
+    setFileData(event, entity, field, isImage) {
+        this.dataUtils.setFileData(event, entity, field, isImage);
+    }
+
+    clearInputImage(field: string, fieldContentType: string, idInput: string) {
+        this.dataUtils.clearInputImage(this.userExtra, this.elementRef, field, fieldContentType, idInput);
     }
 }
